7e5af5153e0b33bae4118f0f08c504f4d74a090c
Pilot Alex <gumery.alexandre@gmail.com>
2023-06-19 17:55:11 +0200

    Added files

---
+++ sharp logger/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        SharpLogger logger = new SharpLogger("./out.md", true);
+
+        logger.Log("Let's take a look at it! 👀", LogLevel.INFO);
+        logger.Log("Let's ignore that 😴", LogLevel.WARNING);
+        logger.Log("🚨 Red alert", LogLevel.ERROR);
+        logger.Log("Oh no 😩", LogLevel.FATAL);
+
+        logger.Log("I can do more, {0} + {1} = {2}", LogLevel.INFO, "🌱", "💦", "🌽");
+
+        // Close the logger
+        logger.Close();
+
+        Console.WriteLine("Logging complete. Press any key to exit.");
+        Console.ReadKey();
+    }
+}
---
+++ sharp logger/SharpLogger.cs
@@ -0,0 +1,51 @@
+﻿/****************************************************************************\
+ * Pilot Alex, 2022-2023, All rights reserved. Copyright (c) 2023.
+ * Made by A.G. under the username of Pilot Alex.
+\****************************************************************************/
+
+using System;
+using System.IO;
+
+enum LogLevel
+{
+    INFO,
+    WARNING,
+    ERROR,
+    FATAL
+}
+
+class SharpLogger : IDisposable
+{
+    private StreamWriter writer;
+
+    public SharpLogger(string savePath, bool append)
+    {
+        this.writer = new StreamWriter(savePath, append);
+    }
+
+    public void Log(string message, LogLevel level, params object[] args)
+    {
+        string formattedMessage = string.Format(message, args);
+        string logEntry = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{level}] {formattedMessage}";
+
+        writer.WriteLine(logEntry);
+        writer.Flush();
+    }
+
+    public void Close()
+    {
+        writer.Close();
+    }
+
+    public void Clear()
+    {
+        writer.Close();
+        File.WriteAllText(writer.BaseStream.Position.ToString(), string.Empty);
+        writer = new StreamWriter(writer.BaseStream);
+    }
+
+    public void Dispose()
+    {
+        writer.Dispose();
+    }
+}
